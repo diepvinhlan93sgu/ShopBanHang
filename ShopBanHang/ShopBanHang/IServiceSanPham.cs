2027d4d41e8b37ef222e82f10fc05ed43bcb539d
secminh <secminh@gmail.com>
2015-12-03 19:21:09 +0700

    service San pham

---
+++ ShopBanHang/ShopBanHang/IServiceSanPham.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ShopBanHang
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceSanPham" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceSanPham
+    {
+        [OperationContract]
+        List<SanPham> All();
+
+        [OperationContract]
+        SanPham ChiTietSP(int masp);
+
+        [OperationContract]
+        List<SanPham> SanPhamTheoLoai(string loai);
+
+
+        [OperationContract]
+        List<SanPham> TimKiem(string loai);
+
+        [OperationContract]
+        bool XoaSanPham(int masp);
+
+
+        [OperationContract]
+        bool SuaSanPham(int maspht, int masp, string tensp, string madm, decimal gia, string mota, string kichthuoc, string mausac, string hinhanh, string soluong, int trangthai);
+
+        [OperationContract]
+        bool ThemSanPham(int masp, string tensp, string madm, decimal gia, string mota, string kichthuoc, string mausac, string hinhanh, string soluong);
+    }
+}
---
+++ ShopBanHang/ShopBanHang/ServiceSanPham.svc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ShopBanHang
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceSanPham" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceSanPham.svc or ServiceSanPham.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceSanPham : IServiceSanPham
+    {
+        private ShopEntities db = new ShopEntities();
+        public List<SanPham> All()
+        {
+            // return this.db.SanPhams.ToList();
+            var sptl = (from p in db.SanPhams where p.TrangThai == 0 select p);
+            return sptl.ToList();
+        }
+
+        public SanPham ChiTietSP(int masp)
+        {
+            return this.db.SanPhams.Single(sp => sp.MaSP == masp);
+        }
+
+        public List<SanPham> SanPhamTheoLoai(string loai)
+        {
+            var sptl = (from p in db.SanPhams where p.MaDM.StartsWith(loai) && p.TrangThai == 0 select p);
+            return sptl.ToList();
+        }
+
+        public List<SanPham> TimKiem(string id)
+        {
+            var product = (from p in db.SanPhams select p).ToList();
+            product = product.Where(a => a.TenSP.ToLower().Contains(id)).ToList();
+            return product;
+        }
+
+        public bool XoaSanPham(int masp)
+        {
+            try
+            {
+                SanPham sp = db.SanPhams.SingleOrDefault(p => p.MaSP == masp);
+                db.SanPhams.Remove(sp);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool SuaSanPham(int maspht, int masp, string tensp, string madm, decimal gia, string mota, string kichthuoc, string mausac, string hinhanh, string soluong, int trangthai)
+        {
+
+            try
+            {
+                SanPham sp = db.SanPhams.First(p => p.MaSP == maspht);
+                sp.TenSP = tensp;
+                sp.MaDM = madm;
+                sp.Gia = gia;
+                sp.MoTa = mota;
+                sp.KichThuoc = kichthuoc;
+
+                sp.MauSac = mausac;
+
+                sp.HinhAnh = hinhanh;
+                sp.SoLuong = soluong;
+                sp.TrangThai = trangthai;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool ThemSanPham(int masp, string tensp, string madm, decimal gia, string mota, string kichthuoc, string mausac, string hinhanh, string soluong)
+        {
+
+            try
+            {
+                SanPham sp = new SanPham();
+                sp.TenSP = tensp;
+                sp.MaDM = madm;
+                sp.Gia = gia;
+                sp.MoTa = mota;
+                sp.KichThuoc = kichthuoc;
+                sp.MauSac = mausac;
+                sp.HinhAnh = hinhanh;
+                sp.SoLuong = soluong;
+
+                db.SanPhams.Add(sp);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
