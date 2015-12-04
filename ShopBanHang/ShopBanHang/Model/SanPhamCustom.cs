b806454c4b0b6a2b9fbd999071a95843da76efc6
secminh <secminh@gmail.com>
2015-12-04 11:51:08 +0700

    Create ServiceSPcustom and add Service References SPcustom

---
+++ ShopBanHang/ShopBanHang/Model/SanPhamCustom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopBanHang
+{
+    public class SanPhamCustom
+    {
+        public string TenDM { get; set; }
+
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public string MaDM { get; set; }
+        public Nullable<decimal> Gia { get; set; }
+        public string MoTa { get; set; }
+        public string KichThuoc { get; set; }
+        public string MauSac { get; set; }
+        public string HinhAnh { get; set; }
+        public string SoLuong { get; set; }
+        public int? TrangThai { get; set; }
+    }
+}
---
+++ ShopBanHang/ShopBanHang/ServiceSanPhamCustom.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ShopBanHang
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceSanPhamCustom" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceSanPhamCustom.svc or ServiceSanPhamCustom.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceSanPhamCustom : IServiceSanPhamCustom
+    {
+        private ShopEntities db = new ShopEntities();
+        public List<SanPhamCustom> All()
+        {
+            var ctsp = (from p in db.SanPhams
+                        join q in db.DanhMucSPs on p.MaDM equals q.MaDM
+                        
+
+                        select new SanPhamCustom
+                        {
+                            MaSP = p.MaSP,
+                            TenSP = p.TenSP,
+                            SoLuong = p.SoLuong,
+                            Gia = decimal.Ceiling((decimal)p.Gia + 0),
+                            MaDM = p.MaDM,
+                            TenDM = q.TenDM,
+                            MoTa = p.MoTa,
+                            KichThuoc = p.KichThuoc,
+                            MauSac = p.MauSac,
+                            HinhAnh= p.HinhAnh,
+                            TrangThai = p.TrangThai,
+                           
+                        }).ToList();
+
+            return ctsp;
+        }
+    }
+}
