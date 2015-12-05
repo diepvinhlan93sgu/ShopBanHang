e83d6f240dfb8f6560660a215edd84832e5bdd61
Chuong <trinhngocchuongsgu@gmail.com>
2015-12-05 12:20:41 +0700

    Create FormQLNhapHang, add ServiceReferenceNhapHang, add class CTPhieuNhapCustom in Model

---
+++ ShopBanHang/ShopWinForm/FormQLNhapHang.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ShopWinForm
+{
+    partial class FormQLNhapHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnThemMoi = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dvPhieunhap = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_xemChiTiet = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.col_xoaPN = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dvPhieunhap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(285, 334);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(100, 39);
+            this.btnThoat.TabIndex = 11;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnThemMoi
+            // 
+            this.btnThemMoi.Location = new System.Drawing.Point(122, 334);
+            this.btnThemMoi.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThemMoi.Name = "btnThemMoi";
+            this.btnThemMoi.Size = new System.Drawing.Size(100, 38);
+            this.btnThemMoi.TabIndex = 10;
+            this.btnThemMoi.Text = "Thêm mới";
+            this.btnThemMoi.UseVisualStyleBackColor = true;
+            this.btnThemMoi.Click += new System.EventHandler(this.btnThemMoi_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dvPhieunhap);
+            this.groupBox1.Location = new System.Drawing.Point(25, 54);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(471, 272);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách phiếu nhập hàng";
+            // 
+            // dvPhieunhap
+            // 
+            this.dvPhieunhap.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvPhieunhap.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.col_xemChiTiet,
+            this.col_xoaPN});
+            this.dvPhieunhap.Location = new System.Drawing.Point(8, 21);
+            this.dvPhieunhap.Margin = new System.Windows.Forms.Padding(4);
+            this.dvPhieunhap.Name = "dvPhieunhap";
+            this.dvPhieunhap.Size = new System.Drawing.Size(454, 230);
+            this.dvPhieunhap.TabIndex = 0;
+            this.dvPhieunhap.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvPhieunhap_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "MaPN";
+            this.Column1.HeaderText = "Mã phiếu nhập";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 110;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "NgayNhap";
+            this.Column2.HeaderText = "Ngày nhập";
+            this.Column2.Name = "Column2";
+            // 
+            // col_xemChiTiet
+            // 
+            this.col_xemChiTiet.HeaderText = "Xem chi tiết";
+            this.col_xemChiTiet.Name = "col_xemChiTiet";
+            this.col_xemChiTiet.Text = "Xem chi tiết";
+            this.col_xemChiTiet.UseColumnTextForLinkValue = true;
+            // 
+            // col_xoaPN
+            // 
+            this.col_xoaPN.HeaderText = "Xóa";
+            this.col_xoaPN.Name = "col_xoaPN";
+            this.col_xoaPN.Text = "Xóa";
+            this.col_xoaPN.UseColumnTextForLinkValue = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(80, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Danh sách phiếu nhập hàng";
+            // 
+            // FormQLNhapHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 385);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThemMoi);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormQLNhapHang";
+            this.Text = "FormQLNhapHang";
+            this.Load += new System.EventHandler(this.FormQLNhapHang_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dvPhieunhap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnThemMoi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dvPhieunhap;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewLinkColumn col_xemChiTiet;
+        private System.Windows.Forms.DataGridViewLinkColumn col_xoaPN;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ShopBanHang/ShopWinForm/FormQLNhapHang.cs
@@ -0,0 +1,77 @@
+﻿using ShopWinForm.ServiceReferenceNhapHang;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopWinForm
+{
+    public partial class FormQLNhapHang : Form
+    {
+        private ServiceNhapHangClient svnh = new ServiceNhapHangClient();
+        List<WFNhapHang> listphieunhap = new List<WFNhapHang>();
+        public static int _mapn;
+        public static DateTime _ngay;
+        public FormQLNhapHang()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FormQLNhapHang_Load(object sender, EventArgs e)
+        {
+            listphieunhap = svnh.All().ToList();
+            dvPhieunhap.AutoGenerateColumns = false;
+            dvPhieunhap.DataSource = listphieunhap;
+        }
+
+        private void btnThemMoi_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dvPhieunhap_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dvPhieunhap.Columns["col_xoaPN"].Index)
+            {
+                // int pn = dvPhieunhap.datakeys Rows[e.RowIndex];
+                int pn = Int32.Parse(dvPhieunhap.Rows[e.RowIndex].Cells[0].Value.ToString());
+                if (DialogResult.Yes == MessageBox.Show("Bạn có muốn xóa không!", "Lưu ý", MessageBoxButtons.YesNo))
+                {
+                    try
+                    {
+                        svnh.XoaPN(pn);
+                        //pn.Delete();
+                        dvPhieunhap.DataSource = null;
+                        dvPhieunhap.DataSource = svnh.All();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Không xóa được: " + ex.Message, "Loi");
+                    }
+                }
+            }
+            else if
+                (e.ColumnIndex == dvPhieunhap.Columns["col_xemChiTiet"].Index)
+            {
+                int pn = Int32.Parse(dvPhieunhap.Rows[e.RowIndex].Cells[0].Value.ToString());
+                DateTime pn1 = DateTime.Parse(dvPhieunhap.Rows[e.RowIndex].Cells[1].Value.ToString());
+                _mapn = pn;
+                _ngay = pn1;
+                FormCTNhapHang frm = new FormCTNhapHang();
+
+                this.Close();
+                frm.Show();
+            }
+        }
+    }
+}
