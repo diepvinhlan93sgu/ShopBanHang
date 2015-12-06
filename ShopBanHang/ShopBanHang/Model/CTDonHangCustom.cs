4731ca76eedd582538e73207fa172b01a9fc6ccc
nguyenhuuhieu7 <nguyenhuuhieu7@gmail.com>
2015-12-06 19:30:33 +0700

    add ServiceCTDonHangCustom

---
+++ ShopBanHang/ShopBanHang/Model/CTDonHangCustom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopBanHang
+{
+    public class CTDonHangCustom
+    {
+        public int? MaSP { get; set; }
+        public int? ID { get; set; }
+        public int? MaDonHang { get; set; }
+        public int? SoLuong { get; set; }
+        public string TenSP { get; set; }
+        public decimal? Gia { get; set; }
+        public decimal? ThanhTien { get; set; }
+    }
+}
---
+++ ShopBanHang/ShopBanHang/ServiceCTDonHangCustom.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ShopBanHang
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceCTDonHangCustom" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceCTDonHangCustom.svc or ServiceCTDonHangCustom.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceCTDonHangCustom : IServiceCTDonHangCustom
+    {
+        private ShopEntities db = new ShopEntities();
+        public List<CTDonHangCustom> ChiTietDonHang(int madonhang)
+        {
+            var cthd = (from p in db.CTDonHangs
+                        join q in db.SanPhams on p.MaSP equals q.MaSP
+                        //   var sanpham =    (from p in db.SanPhams q in db.DanhMucSPs
+                        where p.MaDonHang == madonhang
+
+                        select new CTDonHangCustom
+                        {
+                            MaSP = p.MaSP,
+                            ID = p.ID,
+                            MaDonHang = p.MaDonHang,
+                            SoLuong = p.SoLuong,
+                            TenSP = q.TenSP,
+                            Gia = decimal.Ceiling((decimal)p.Gia + 0),
+                            ThanhTien = decimal.Ceiling((decimal)p.ThanhTien + 0)
+                        }).ToList();
+
+            return cthd;
+
+
+        }
+    }
+}
